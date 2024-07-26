e96b09eeaa877b3f06073f29b39db5a58511fa28
vaishali bhardwaj <vaishalibhardwaj@vaishalis-MacBook-Pro.local>
2024-07-26 23:45:45 +0530

    pushing all the main files after throughly testing mar24 to july24

--- signHMAC.js
+++ signHMAC.js
@@ -1,32 +1,27 @@
-// sign with default (HMAC SHA256)
 var jwt = require('jsonwebtoken');
 var token = jwt.sign({ foo: 'bar' }, 'eloooo');
 console.log('token:');
 console.log(token);
+//signed with default SHA256
 
-
-
-// verify a token symmetric - synchronous
 var decoded = jwt.verify(token, 'eloooo');
 console.log('--------------');
-console.log('decoded.foo:'); // bar
-console.log(decoded.foo); // bar
+console.log('decoded.foo:'); 
+console.log(decoded.foo); 
 console.log('--------------');
-console.log('decoded:'); // bar
+console.log('decoded:'); 
 console.log(decoded);
 console.log('--------------');
 
 
-// verify a token symmetric - asynchronous
 jwt.verify(token, 'eloooo', function(err, decoded) {
   console.log('--------------');  
-  console.log('decoded.foo:'); // bar
-  console.log(decoded.foo); // bar
+  console.log('decoded.foo:'); 
+  console.log(decoded.foo); 
   console.log('--------------');  
 });
 
 
-// without verification
 decoded = jwt.decode(token, {complete: true});
 console.log('decoded.header:');  
 console.log(decoded.header);
